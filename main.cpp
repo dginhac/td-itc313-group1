11f1a4ef09bf8b50ed283f7b3ded1b7e40df4913
d0m G <dom@ginhac.com>
2021-10-12 09:23:59 +0200

    Initial commit with Makefile and Date class

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @Author: Dominique Ginhac <d0m>
+ * @Date:   12 oct. 2021 - 08h43
+ * @Email:  dginhac@u-bourgogne.fr
+ * @Project: C++ Programming - ESIREM 3A - TD1 Gr1
+ */
+
+#include <iostream>
+#include "date.h"
+
+
+
+int main(int argc, char const *argv[]) {
+
+   date::Date date1(10,12);
+
+   std::cout << "Bye" << std::endl;
+   return 0;
+}
